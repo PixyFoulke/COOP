28c59a68ae2529ba3ad56570cd1c9b2f555d2b5c
layanaburomee <layanaburomeh@outlook.com>
2026-07-08 08:56:52 -0400

    debug

--- hardware/ads.py
+++ hardware/ads.py
@@ -4,15 +4,14 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# Create the I2C bus
+
 i2c = busio.I2C(board.SCL, board.SDA)
 
-# Create ADC object
 ads = ADS.ADS1115(i2c)
 
-# Photoresistors
-pr1 = AnalogIn(ads, ADS.P0)
-pr2 = AnalogIn(ads, ADS.P1)
+
+pr1 = AnalogIn(ads, 0)
+pr2 = AnalogIn(ads, 1)
 
 
 def get_light_values():
@@ -24,10 +23,6 @@ def get_light_voltages():
 
 
 def is_night(threshold=1.0):
-    """
-    Returns True if the average light voltage is below the threshold.
-    Adjust the threshold after testing.
-    """
     avg_voltage = (pr1.voltage + pr2.voltage) / 2
     return avg_voltage < threshold
 
