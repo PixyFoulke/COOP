87cbe99e560147a08328adb1acc5d97e750db6a4
layanaburomee <layanaburomeh@outlook.com>
2026-07-08 08:50:06 -0400

    debug

--- hardware/oled_display.py
+++ hardware/oled_display.py
@@ -28,11 +28,11 @@ def update_status(
     temp_str = f"{temp:.1f}F" if temp is not None else "N/A"
     hum_str = f"{humidity:.0f}%" if humidity is not None else "N/A"
 
-    # Draw OLED screen
-    draw.text((0, 0), coop, fill=255)
+   # Draw OLED screen
+    draw.text((0, 0), f"{coop} {light_state[0]}", fill=255)
     draw.text((0, 16), f"T:{temp_str} H:{hum_str}", fill=255)
     draw.text((0, 32), current_time, fill=255)
-    draw.text((0, 48), f"{light_state} C:{chickens}", fill=255)
+    draw.text((0, 48), f"Chickens:{chickens}", fill=255)
 
     # Push to OLED
     _oled.image(image)
