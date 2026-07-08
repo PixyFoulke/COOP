4d0cd80f7a5242cd41b940b1f4d145aa83808c65
layanaburomee <layanaburomeh@outlook.com>
2026-07-08 08:54:49 -0400

    debug

--- ai/actions.py
+++ ai/actions.py
@@ -1,16 +1,35 @@
 
 # actions that can be taken based on the results of the yolo detection code
 
+last_state = None
+
+
 def trigger_alarm():
-    print("ALARM TRIGGERED: Threat detected!")
+    global last_state
+
+    if last_state != "THREAT":
+        print("ALARM TRIGGERED: Threat detected!")
+
+    last_state = "THREAT"
 
 
 def safe_state():
-    print("SAFE: No threats detected.")
+    global last_state
+
+    if last_state != "SAFE":
+        print("SAFE: No threats detected.")
+
+    last_state = "SAFE"
 
 
 def warning_state():
-    print("WARNING: Unknown objects detected.")
+    global last_state
+
+    if last_state != "WARNING":
+        print("WARNING: Unknown objects detected.")
+
+    last_state = "WARNING"
+
 
 # Future upgrades:
     # Displayed on OLED screen
