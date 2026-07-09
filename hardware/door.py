5bbd2e5c04de4b50d2d71742ee1a72705cb610f3
layanaburomee <layanaburomeh@outlook.com>
2026-07-09 13:00:11 -0400

    door debug

--- hardware/door.py
+++ hardware/door.py
@@ -29,64 +29,28 @@ DOOR_COOLDOWN = 5
 
 def door_open():
 
-    global last_door_command
-    global door_state
-
-    # cooldown protection
-    if time.time() - last_door_command < DOOR_COOLDOWN:
-
-        print("Door command ignored (cooldown)")
-        return
-
-    last_door_command = time.time()
-
     print("OPEN signal")
 
     door_open_pin.value = True
-    time.sleep(1)
 
-    door_open_pin.value = False
-    time.sleep(1)
-
-    door_open_pin.value = True
-    time.sleep(1)
+    time.sleep(20)
 
     door_open_pin.value = False
 
     print("OPEN complete")
 
-    door_state = "open"
-
 
 def door_close():
-
-    global last_door_command
-    global door_state
-
-    # cooldown protection
-    if time.time() - last_door_command < DOOR_COOLDOWN:
-
-        print("Door command ignored (cooldown)")
-        return
-
-    last_door_command = time.time()
 
     print("CLOSE signal")
 
     door_close_pin.value = True
-    time.sleep(1)
 
-    door_close_pin.value = False
-    time.sleep(1)
-
-    door_close_pin.value = True
-    time.sleep(1)
+    time.sleep(20)
 
     door_close_pin.value = False
 
     print("CLOSE complete")
-
-    door_state = "closed"
 
 
 def toggle_door():
