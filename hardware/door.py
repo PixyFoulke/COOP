be5284816004e8cf141dc55223736fb56c9654d5
layanaburomee <layanaburomeh@outlook.com>
2026-07-09 10:29:15 -0400

    door debug

--- hardware/door.py
+++ hardware/door.py
@@ -4,68 +4,28 @@ import board
 import digitalio
 
 
-# Door controller pins
-# OPEN = GPIO 27
-# CLOSE = GPIO 22
-
 door_open_pin = digitalio.DigitalInOut(board.D27)
 door_close_pin = digitalio.DigitalInOut(board.D22)
-
 
 door_open_pin.direction = digitalio.Direction.OUTPUT
 door_close_pin.direction = digitalio.Direction.OUTPUT
 
 
-# Default state
 door_open_pin.value = False
 door_close_pin.value = False
 
 
-door_state = False
+def press_button(pin):
+    pin.value = True
+    time.sleep(0.1)
+    pin.value = False
 
 
 def open_door():
-
-    global door_state
-
-    print("Opening door...")
-
-    door_open_pin.value = True
-    time.sleep(0.2)
-    door_open_pin.value = False
-
-    door_state = True
-
-    print("Door opened.")
+    print("Opening door")
+    press_button(door_open_pin)
 
 
 def close_door():
-
-    global door_state
-
-    print("Closing door...")
-
-    door_close_pin.value = True
-    time.sleep(0.2)
-    door_close_pin.value = False
-
-    door_state = False
-
-    print("Door closed.")
-
-
-def toggle_door():
-
-    global door_state
-
-    if door_state:
-        close_door()
-    else:
-        open_door()
-
-    return door_state
-
-
-def is_door_open():
-
-    return door_state
+    print("Closing door")
+    press_button(door_close_pin)
--- testdoor.py
+++ testdoor.py
@@ -1,11 +1,12 @@
 
-from hardware.door import open_door
+from hardware.door import open_door, close_door
 import time
 
-print("Testing door")
 
+print("Testing open")
 open_door()
 
-time.sleep(3)
+time.sleep(5)
 
-print("Finished")
+print("Testing close")
+close_door()
