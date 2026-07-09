9838ffd9fd033a6cab3f0d1fc00429a32157f34b
layanaburomee <layanaburomeh@outlook.com>
2026-07-09 12:29:04 -0400

    website updates

--- website/js/door.js
+++ website/js/door.js
@@ -1,2 +1,90 @@
 
 console.log("Door page ready");
+
+
+const API = "http://192.168.50.2:5000";
+
+
+
+function openDoor(){
+
+    console.log("Opening door");
+
+
+    document.getElementById("doorStatus").innerHTML =
+    "Opening...";
+
+
+    fetch(
+        API + "/door/open",
+        {
+            method: "POST"
+        }
+    )
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        console.log(data.message);
+
+
+        document.getElementById("doorStatus").innerHTML =
+        "Open command sent";
+
+    })
+
+    .catch(error => {
+
+        console.log(error);
+
+
+        document.getElementById("doorStatus").innerHTML =
+        "Door error";
+
+    });
+
+}
+
+
+
+
+function closeDoor(){
+
+    console.log("Closing door");
+
+
+    document.getElementById("doorStatus").innerHTML =
+    "Closing...";
+
+
+    fetch(
+        API + "/door/close",
+        {
+            method: "POST"
+        }
+    )
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        console.log(data.message);
+
+
+        document.getElementById("doorStatus").innerHTML =
+        "Close command sent";
+
+    })
+
+    .catch(error => {
+
+        console.log(error);
+
+
+        document.getElementById("doorStatus").innerHTML =
+        "Door error";
+
+    });
+
+}
