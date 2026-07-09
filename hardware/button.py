e4d3376e2e0976b2f94690315823616e29b4e9d1
layanaburomee <layanaburomeh@outlook.com>
2026-07-09 10:48:19 -0400

    button debug

--- hardware/button.py
+++ hardware/button.py
@@ -4,60 +4,34 @@ import digitalio
 import time
 import os
 
-button = digitalio.DigitalInOut(board.D14)
+
+button = digitalio.DigitalInOut(board.D17)
+
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
-DOUBLE_CLICK_TIME = 0.5
-HOLD_TIME = 5.0
-
-last_release = 0
-click_count = 0
-
-
-def button_pressed():
-    return not button.value
-
 
 def reboot_pi():
+
     os.system("sudo reboot")
 
 
 def get_button_action():
-    global last_release
-    global click_count
 
-    if button_pressed():
+    # wait for button press
+    if not button.value:
 
-        start = time.time()
-
-        while button_pressed():
-
-            if time.time() - start >= HOLD_TIME:
-
-                while button_pressed():
-                    time.sleep(0.05)
-
-                click_count = 0
-                return "restart"
-
-            time.sleep(0.05)
+        print("Button detected")
 
         # debounce
-        time.sleep(0.05)
+        time.sleep(0.1)
 
-        click_count += 1
-        last_release = time.time()
+        # wait until button is released
+        while not button.value:
+            time.sleep(0.05)
 
-    if click_count > 0:
-        if time.time() - last_release > DOUBLE_CLICK_TIME:
+        print("Button released")
 
-            if click_count == 1:
-                action = "toggle"
-            else:
-                action = "alarm"
-
-            click_count = 0
-            return action
+        return "toggle"
 
     return None
