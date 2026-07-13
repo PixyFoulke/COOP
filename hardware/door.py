cd5bf53f374b5eb63c62198779b7c71c50fa9302
layanaburomee <layanaburomeh@outlook.com>
2026-07-13 12:52:20 -0400

    door debug

--- hardware/door.py
+++ hardware/door.py
@@ -4,27 +4,14 @@ import board
 import digitalio
 
 
-# GPIO PINS
 door_open_pin = digitalio.DigitalInOut(board.D27)
 door_close_pin = digitalio.DigitalInOut(board.D17)
-
 
 door_open_pin.direction = digitalio.Direction.OUTPUT
 door_close_pin.direction = digitalio.Direction.OUTPUT
 
-
-# IDLE STATE
 door_open_pin.value = False
 door_close_pin.value = False
-
-
-# TRACK STATE
-door_state = "closed"
-
-
-# PREVENT RAPID COMMANDS
-last_door_command = 0
-DOOR_COOLDOWN = 5
 
 
 def door_open():
@@ -32,9 +19,7 @@ def door_open():
     print("OPEN signal")
 
     door_open_pin.value = True
-
-    time.sleep(20)
-
+    time.sleep(1)
     door_open_pin.value = False
 
     print("OPEN complete")
@@ -45,25 +30,7 @@ def door_close():
     print("CLOSE signal")
 
     door_close_pin.value = True
-
-    time.sleep(20)
-
+    time.sleep(1)
     door_close_pin.value = False
 
     print("CLOSE complete")
-
-
-def toggle_door():
-
-    if door_state == "closed":
-
-        door_open()
-
-    else:
-
-        door_close()
-
-
-def is_door_open():
-
-    return door_state == "open"
