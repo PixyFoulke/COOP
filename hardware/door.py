f3194d95b04335112d41cb62f57894329da5b208
layanaburomee <layanaburomeh@outlook.com>
2026-07-27 09:46:30 -0400

    restore 20 second door signal

--- hardware/door.py
+++ hardware/door.py
@@ -4,14 +4,27 @@ import board
 import digitalio
 
 
+# GPIO PINS
 door_open_pin = digitalio.DigitalInOut(board.D27)
-door_close_pin = digitalio.DigitalInOut(board.D22)
+door_close_pin = digitalio.DigitalInOut(board.D17)
+
 
 door_open_pin.direction = digitalio.Direction.OUTPUT
 door_close_pin.direction = digitalio.Direction.OUTPUT
 
+
+# IDLE STATE
 door_open_pin.value = False
 door_close_pin.value = False
+
+
+# TRACK STATE
+door_state = "closed"
+
+
+# PREVENT RAPID COMMANDS
+last_door_command = 0
+DOOR_COOLDOWN = 5
 
 
 def door_open():
@@ -19,7 +32,9 @@ def door_open():
     print("OPEN signal")
 
     door_open_pin.value = True
-    time.sleep(8)
+
+    time.sleep(20)
+
     door_open_pin.value = False
 
     print("OPEN complete")
@@ -30,7 +45,25 @@ def door_close():
     print("CLOSE signal")
 
     door_close_pin.value = True
-    time.sleep(8)
+
+    time.sleep(20)
+
     door_close_pin.value = False
 
     print("CLOSE complete")
+
+
+def toggle_door():
+
+    if door_state == "closed":
+
+        door_open()
+
+    else:
+
+        door_close()
+
+
+def is_door_open():
+
+    return door_state == "open"
